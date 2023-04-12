83562124cc3b7696d03c8db17f250ab692b7bdc9
Marian Israel <marianwadia55@gmail.com>
2023-04-12 11:08:19 +0200

    styles and markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import './App.css';
 
 function App() {
+  const digits = [];
+
+  const createDigits = () => {
+    for(let i = 1; i <10 ; i++) {
+      digits.push(i);
+    }
+    return digits;
+  }
   return (
     <div className="App">
-
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span>0
+        </div>
+          <div className="operators">
+            <button>/</button>
+            <button>x</button>
+            <button>+</button>
+            <button>-</button>
+            <button>DEL</button>
+          </div>
+          <div className="digits">
+              {createDigits().map((digit, index)=>{
+                return <button key={index}>{digit}</button>;
+              })} 
+              <button>0</button>
+              <button>.</button>
+              <button>=</button>
+            </div>  
+      </div>
     </div>
   );
 }
